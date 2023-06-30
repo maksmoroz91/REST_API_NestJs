3155a2a00238e02e3eada6caf5a82ba352ad7d9e
maksmoroz91 <dlkontakta7@gmail.com>
2023-06-30 11:15:58 +0300

    added a check for the role in the DB

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { CreateRoleDto } from "./dto/create-role.dto";
@@ -10,6 +10,11 @@ export class RolesService {
     constructor(@InjectRepository(RoleEntity) private readonly rolesRepository: Repository<RoleEntity>) {}
 
     async createRole(dto: CreateRoleDto): Promise<RoleEntity> {
+        const role = await this.getRoleByValue(dto.value);
+
+        if (role) {
+            throw new BadRequestException('Такая роль уже есть')
+        }
         const newRole = await this.rolesRepository.create(dto);
 
         return await this.rolesRepository.save(newRole);
